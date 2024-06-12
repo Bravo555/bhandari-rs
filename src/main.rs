056e21123d0b2514114ec346d190ca7ae23088da
Marcel Guzik <mguzik555@gmail.com>
2024-06-12 10:39:02 +0200

    working bhandari on graph 2

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
+use clap::{arg, command, Parser};
 use pathfinding::prelude::*;
-use std::fs;
+use std::{collections::HashMap, fs};
 
-use anyhow::{ensure, Context};
+use anyhow::Context;
+
+#[derive(Debug, Clone, Parser)]
+#[command()]
+struct Args {
+    #[arg()]
+    file: String,
+
+    #[arg()]
+    start: u32,
+
+    #[arg()]
+    to: u32,
+
+    #[arg()]
+    k: usize,
+}
 
 fn main() -> anyhow::Result<()> {
-    let edges: anyhow::Result<Vec<_>> = fs::read_to_string("graphs/01.edges")?
+    let args = Args::parse();
+    let edges: anyhow::Result<Vec<_>> = fs::read_to_string(args.file)?
         .lines()
-        .filter(|s| !s.is_empty())
+        .filter(|line| !line.is_empty() && !line.starts_with("//"))
         .map(parse_edge)
         .collect();
-    let mut edges = edges?;
+    let edges = edges?;
 
-    let result = bhandari(&mut edges, 1, 5, 3).context("getting path from 1 to 5")?;
+    let result = bhandari(&edges, args.start, args.to, args.k).context("getting disjoint paths")?;
 
-    println!("{result:#?}");
+    println!("{result:?}");
 
     Ok(())
 }
 
-fn bhandari(_graph: &[Edge], start: u32, to: u32, k: usize) -> anyhow::Result<Vec<Vec<u32>>> {
+fn bhandari(_graph: &[Edge], start: u32, end: u32, k: usize) -> anyhow::Result<Vec<Vec<u32>>> {
     // dijkstra calls a function at each step to get list of next nodes it goes to, so transform our
     // edge list to lambda that returns `to` nodes for a given node
     let shortest_path = {
@@ -31,8 +49,8 @@ fn bhandari(_graph: &[Edge], start: u32, to: u32, k: usize) -> anyhow::Result<Ve
         };
 
         // find shortest path P_1 from s to t
-        let (shortest_path, cost) =
-            dijkstra(&start, successors, |current_node| *current_node == to)
+        let (shortest_path, _cost) =
+            dijkstra(&start, successors, |current_node| *current_node == end)
                 .context("this graph doesn't contain such path")?;
 
         shortest_path
@@ -41,24 +59,26 @@ fn bhandari(_graph: &[Edge], start: u32, to: u32, k: usize) -> anyhow::Result<Ve
     let mut paths: Vec<Vec<u32>> = Vec::with_capacity(k);
     paths.push(shortest_path);
 
-    for i in 0..(k - 1) {
+    for _ in 0..(k - 1) {
         // if node-disjoint path split the intermediate nodes of all Px where x < i
         // we use link-disjoint, so skip
 
         // Replace each link of all P_x where x < i with a reverse link of inverted link weight in the original graph
-        let mut graph = _graph.to_vec();
+        let mut graph: HashMap<(u32, u32), i32> = HashMap::from_iter(
+            _graph
+                .iter()
+                .map(|edge| ((edge.from, edge.to), edge.weight)),
+        );
+
         for path in &paths {
             for link in path.windows(2) {
                 let from = link[0];
                 let to = link[1];
 
-                let link = graph
-                    .iter_mut()
-                    .find(|link| link.from == from && link.to == to)
+                let (_, weight) = graph
+                    .remove_entry(&(from, to))
                     .expect("link should be present");
-                link.from = to;
-                link.to = from;
-                link.weight = -link.weight;
+                graph.insert((to, from), -weight);
             }
         }
 
@@ -66,12 +86,12 @@ fn bhandari(_graph: &[Edge], start: u32, to: u32, k: usize) -> anyhow::Result<Ve
         let successors = |current_node: &u32| {
             graph
                 .iter()
-                .filter(|edge| edge.from == *current_node)
-                .map(|&Edge { to, weight, .. }| (to, weight))
+                .filter(|((from, _), _)| *current_node == *from)
+                .map(|((_, to), weight)| (*to, *weight))
                 .collect::<Vec<_>>()
         };
         let (shortest_path, _cost) =
-            dijkstra(&start, successors, |current_node| *current_node == to)
+            dijkstra(&start, successors, |current_node| *current_node == end)
                 .context("this graph doesn't contain such path")?;
 
         paths.push(shortest_path);
@@ -100,19 +120,31 @@ fn bhandari(_graph: &[Edge], start: u32, to: u32, k: usize) -> anyhow::Result<Ve
             }
         }
 
-        for path in &mut paths {
-            let links = path.windows(2).map(|l| (l[0], l[1])).collect::<Vec<_>>();
-            for link in links {
-                if !unique_links.contains(&link) {
-                    let to = link.1;
-                    let pos = path
+        let starting_links = unique_links
+            .iter()
+            .filter(|(from, _)| *from == start)
+            .copied()
+            .collect::<Vec<_>>();
+
+        paths = starting_links
+            .iter()
+            .map(|(start, starting_next)| {
+                let mut current_node = *starting_next;
+                let mut path = vec![*start, current_node];
+
+                while current_node != end {
+                    let pos = unique_links
                         .iter()
-                        .position(|node| *node == to)
-                        .expect("path should contain node");
-                    path.remove(pos);
+                        .position(|(from, _)| current_node == *from)
+                        .expect("should exist");
+                    let (_, next) = unique_links.remove(pos);
+
+                    path.push(next);
+                    current_node = next;
                 }
-            }
-        }
+                path
+            })
+            .collect::<Vec<_>>();
     }
 
     Ok(paths)
@@ -130,12 +162,7 @@ fn parse_edge(line: &str) -> anyhow::Result<Edge> {
 
     let from = parts.next().context("no starting node")?.parse()?;
 
-    let edge = parts.next().context("no edge")?;
-    let (weight, rest) = edge
-        .split_once(|c: char| !c.is_ascii_digit())
-        .context("no weight")?;
-    ensure!(rest == ">");
-    let weight = weight.parse()?;
+    let weight = parts.next().context("no weight")?.parse()?;
 
     let to = parts.next().context("no finish node")?.parse()?;
 
